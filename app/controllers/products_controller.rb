b4bd3d91b0376f11ebe05c97d7b4a5162c504c4a
Jeevesh Kumar <jeeveshkumar202@gmail.com>
2024-03-14 17:14:09 +0530

    new file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,27 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @products = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price, :author, :category, :description, :quantity)
+    end
 end
