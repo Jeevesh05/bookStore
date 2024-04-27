11b982ffe0f62e69096e424e10a34f56bf937794
hrishabh singh <hrishabhsingh.w@gmail.com>
2024-04-27 18:53:19 +0530

    review and checkout and orders css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_131804) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_27_124827) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -77,6 +77,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_25_131804) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "status"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
